28db7418d06f61202ccda04eaf18b7c6c225bf57
Yuriy Skrypnyk <skrypnyk81@gmail.com>
2023-01-04 19:05:04 +0100

    Work app

---
+++ Collection/getCollection.go
@@ -0,0 +1,8 @@
+package Collection
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("myGoappDB").Collection("Posts")
+	return collection
+}
