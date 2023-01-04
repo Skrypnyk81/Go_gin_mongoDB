d80e60c80cd58f213509c122b2f2d06bba6350cd
Yuriy Skrypnyk <skrypnyk81@gmail.com>
2023-01-04 20:21:15 +0100

    Restore app

--- Collection/getCollection.go
+++
@@ -1,8 +0,0 @@
-package Collection
-
-import "go.mongodb.org/mongo-driver/mongo"
-
-func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	collection := client.Database("myGoappDB").Collection("Posts")
-	return collection
-}
--- model/model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Posts struct {
-	ID      primitive.ObjectID `json:"postId" bson:"id"`
-	Title   string             `json:"title" bson:"title"`
-	Article string             `json:"article" bson:"article"`
-}
